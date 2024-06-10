80d5e3d29461a01bba57012d554476a159ca0c38
hoevans <hoevans87@gmail.com>
2024-06-10 12:02:22 -0500

    removed terraform block

--- providers.tf
+++ providers.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "5.33.0"
-    }
-  }
-}
-
 provider "google" {
   project = "terraform-training-425614"
   region  = "us-west1"
