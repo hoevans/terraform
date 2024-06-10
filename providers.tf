c81588b73bf430f2ee873451a7b7ff1353a53f49
hoevans <hoevans87@gmail.com>
2024-06-10 13:38:42 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,14 +1,5 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "5.33.0"
-    }
-  }
-}
-
 provider "google" {
-    project = "terraform-training-425614"
-    region = "us-west1"
-    zone = "us-west1-a"
+  project = "terraform-training-425614"
+  region = "us-west1"
+  zone = "us-west1-a"
 }
