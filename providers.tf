b3a29017a2812706a8442fe92cd24238715bbab1
hoevans <hoevans87@gmail.com>
2024-06-10 12:41:02 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,25 @@
+terraform {
+    workspaces {
+      name = "terraform"
+    }
+  }
+
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "google" {
+  credentials = var.google_credentials
   project = "terraform-training-425614"
   region  = "us-west1"
   zone    = "us-west1-a"
+}
+
+variable "google_credentials" {
+  description = "Google Cloud credentials in JSON format"
+  type = string
 }
